8c8ae295d8af568dcce6640b856eb0ad59b022d6
tsuyuzakitatsuya <tsuyuzaki.tatsuya@engi-st.jp>
2020-06-18 14:04:50 +0900

    クラスで作成できるかテスト

--- curveAntenna.py
+++ curveAntenna.py
@@ -1,43 +1,48 @@
 import pymel.core as pm
 
 def main():
+
     sels = pm.selected()
+    createCurve.name(sels)
+    class createCurve:
+        def name(self,sels):
+            if sels == []:
+                trsName = ''
+                ctrlName = ''
+                createCurve.trsSetting(trsName,ctrlName)
+            else:
+                for sel in sels:
+                    part = sel.split("_")
+                    trsName = sel.replace( '_'.join(['null',part[0],scene,'C'] ))
+                    ctrlName = sel.replace( '_'.join(['ctrl',part[0],scene,'C'] ))
+                    createCurve.trsSetting(trsName,ctrlName)
+
+        def trsSetting(self,trsName,ctrlName):
+            trs = pm.createNode( 'transform', n= trsName)
+            ctrl = curveAntenna(ctrlName)
+
+            pm.parent(ctrl, trs)
+            pm.parent(trs, sel)
+            
+            pm.move( 0, 0, 0 ,ls=True)
+            pm.rotate( 0, 0, 0 , os=True)
+            pm.parent(trs ,w = True)
+
+        def curveAntenna(self,name):
+            a1 = (0,0,0)
+            a2 = (0,3,0)
+            a3 = (0,5,0)
+            bxf =(1,4,0)
+            bxb =(-1,4,0)
+            bzf =(0,4,1)
+            bzb =(0,4,-1)
+
+            pm.curve( d=1,p=[a1,a3,bxf,a2,bxb,a3,bzf,a2,bzb,a3,bxf,bxb,bzf,bzb,bxf,bzf,bzb,bxb], n=name )
+            return name
+
+
     
-    allSel = pm.select(hi = True, ado = True)
-    allCheck = pm.selected()
-    pm.select(cl = True)
+
     
-    if sels == []:
-        pm.error('Please select an object')
-    
-    for sel in sels:
-        part = sel.split("_")
-        trsName = sel.replace( part[0],'null')
-        ctrlName = sel.replace( part[0],'ctrl')
+
         
-        for check in allCheck:
-            if check == ctrlName or check == trsName:
-                pm.error('It stopped because of the name cast')
-        
-        trs = pm.createNode( 'transform', n= trsName)
-
-        ctrl = curveAntenna(ctrlName)
-
-        pm.parent(ctrl, trs)
-        pm.parent(trs, sel)
-        
-        pm.move( 0, 0, 0 ,ls=True)
-        pm.rotate( 0, 0, 0 , os=True)
-        pm.parent(trs ,w = True)
-
-def curveAntenna(name):
-    a1 = (0,0,0)
-    a2 = (0,3,0)
-    a3 = (0,5,0)
-    bxf =(1,4,0)
-    bxb =(-1,4,0)
-    bzf =(0,4,1)
-    bzb =(0,4,-1)
-
-    tmpCurve = pm.curve( d=1,p=[a1,a3,bxf,a2,bxb,a3,bzf,a2,bzb,a3,bxf,bxb,bzf,bzb,bxf,bzf,bzb,bxb], n=name )
-    return name
